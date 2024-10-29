f87b500642514dc8b48df9d7cdcc01983f495529
TingHuang222324 <t99635@gmail.com>
2024-10-29 16:55:30 +0800

    fix:rwd

--- src/App.js
+++ src/App.js
@@ -3,28 +3,28 @@ import notice1 from './assets/images/notice-1.png'
 function App() {
   return (
     <div className='min-h-screen'>
-      <div className="min-h-screen flex flex-col" style={{ backgroundImage: `url('${bg}')` }}>
-        <div className='flex flex-col justify-center items-center '>
+      <div className="flex flex-col min-h-screen" style={{ backgroundImage: `url('${bg}')` }}>
+        <div className='flex flex-col items-center justify-center '>
 
-          <div className='bg-[#22251D] rotate-[-5deg] text-white  py-4 px-5 text-center mt-28 w-[60%] lg:w-[20%] mb-14'>
+          <div className='bg-[#22251D] rotate-[-5deg] text-white  py-4 px-5 text-center mt-28 w-[60%] md:w-[30%] mb-14'>
             <div className='rotate-[5deg] font-zenKurenaido text-4xl '>
               白金瑪麗
             </div>
           </div>
-          <div className='font-roboto text-[40px] text-center mb-14'>已蒐集到的店家清單</div>
-          <div className='font-roboto text-4xl text-center mb-5'>10/12  Bar Mood</div>
-          <div className='font-roboto text-4xl text-center'>11/29 To Infinity and Beyond</div>
+          <div className='text-3xl text-enter md:text-4xl font-roboto mb-14'>已蒐集到的店家清單</div>
+          <div className='mb-5 text-2xl text-center md:text-4xl font-roboto'>10/12  Bar Mood</div>
+          <div className='text-2xl text-center md:text-4xl font-roboto'>11/29 To Infinity and Beyond</div>
         </div>
-        <div className='flex-1 flex flex-col items-end justify-center mt-56 lg:mt-40 pb-16 text-center'>
-          <button className="flex relative flex-col items-center justify-center shadow-[5px_5px_0px_0px_#000000]   max-w-full transform rotate-[-5deg] bg-white h-[71px] w-[80%] lg:w-[50%]  mx-auto ">
-            <div className="box-border font-roboto relative shrink-0 self-center h-auto text-3xl ">
+        <div className='flex flex-col items-end justify-end flex-1 py-16 text-center md:mt-0'>
+          <button className="flex relative flex-col items-center justify-center shadow-[5px_5px_0px_0px_#000000]   max-w-full transform rotate-[-5deg] bg-white h-[71px] w-[80%]  mx-auto ">
+            <div className="box-border relative self-center h-auto text-3xl font-roboto shrink-0 ">
               點這集點
             </div>
           </button>
-          <div className='flex justify-center  mt-5 mx-auto'>
-            <div className='bg-[#2A2B23] font-roboto text-white text-xl  lg:text-3xl rotate-[10deg] px-2  flex items-center shadow-[5px_5px_0px_0px_#fff]'>LINE POINTS <br />兌換</div>
-            <div className='text-[#2A2B23] font-roboto bg-white  text-xl  lg:text-3xl  px-2 flex items-center shadow-[5px_5px_0px_0px_#000000]'>活動店家</div>
-            <div className='bg-[#2A2B23] font-roboto text-white  text-xl  lg:text-3xl rotate-[-10deg] px-2 flex items-center shadow-[5px_5px_0px_0px_#fff]'>已集點店家</div>
+          <div className='flex justify-center mx-auto mt-5'>
+            <div className='bg-[#2A2B23] font-roboto text-white  text-base md:text-3xl rotate-[10deg] w-28 md:w-60  flex items-center justify-center shadow-[5px_5px_0px_0px_#fff] '>LINE POINTS <br />兌換</div>
+            <div className='text-[#2A2B23] font-roboto bg-white  text-base  md:text-3xl   flex items-center justify-center shadow-[5px_5px_0px_0px_#000000] w-24 md:w-56'>活動店家</div>
+            <div className='bg-[#2A2B23] font-roboto text-white  text-base  md:text-3xl rotate-[-10deg] flex items-center justify-center shadow-[5px_5px_0px_0px_#fff] w-28 md:w-56'>已集點店家</div>
           </div>
         </div>
     
@@ -34,7 +34,7 @@ function App() {
 
 
       <footer className="" >
-        <img className='h-full w-full' srcSet={notice1} alt='酒後不開車'/>
+        <img className='w-full h-full' srcSet={notice1} alt='酒後不開車'/>
 
       </footer>
     </div>
